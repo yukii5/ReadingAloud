e0ca0268b8d6d2c9df34efd20306a8f2ff049086
yukii5 <assault03@gmail.com>
2021-06-21 14:57:02 +0900

    README修正

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -7,22 +7,17 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'ReadingAloud') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    @yield(('js'))
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ '/css/app.css' }}" rel="stylesheet">
-    <link href="{{ '/css/utility.css' }}" rel="stylesheet">
-    @yield('css')
 </head>
 <body>
     <div id="app">
@@ -45,21 +40,21 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <!-- @if (Route::has('login')) -->
+                            @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('ログイン') }}</a>
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
-                            <!-- @endif -->
+                            @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('新規登録') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
@@ -69,8 +64,7 @@
                                         {{ __('Logout') }}
                                     </a>
 
-                                    <!-- <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none"> -->
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
                                 </div>
@@ -81,41 +75,9 @@
             </div>
         </nav>
 
-        <main class="main">
-        @if(session('success'))
-            <div class="alert alert-success" role="alert">
-              {{ session('success') }}
-            </div>
-        @endif
-          <div class="row" style='height: 92vh;'>
-            <div class="col-md-2 p-0">
-              <div class="card h-100">
-              <div class="card-header">タグ一覧</div>
-              <div class="card-body py-2 px-4">
-                <a class='d-block' href='/'>全て表示</a>
-        {{-- @foreach($tags as $tag)
-                  <a href="/?tag={{ $tag['name'] }}" class='d-block'>{{ $tag['name'] }}</a>
-        @endforeach --}}
-                </div>
-              </div>
-            </div>
-                
-            <div class="col-md-4 p-0">
-              <div class="card h-100">
-                <div class="card-header d-flex">メモ一覧 <a class='ml-auto' href='/create'><i class="fas fa-plus-circle"></i></a></div>
-                <div class="card-body p-2">
-                {{-- @foreach($memos as $memo)
-                  <a href="/edit/{{ $memo['id'] }}" class='d-block'>{{ $memo['content'] }}</a>
-        @endforeach --}}
-                </div>
-              </div>    
-            </div> <!-- col-md-3 -->
-            <div class="col-md-6 p-0">
-              @yield('content')
-            </div>
-          </div> <!-- row justify-content-center -->
+        <main class="py-4">
+            @yield('content')
         </main>
     </div>
-    @yield('footer')
 </body>
 </html>
